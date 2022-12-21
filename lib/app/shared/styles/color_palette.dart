f756e15a9d9021bff0f6d1155388b839e01c65fd
stobertonf <stobertonf@gmail.com>
2022-12-21 20:04:02 -0300

    feat(Modular): Creating Color Pallet File.

---
+++ lib/app/shared/styles/color_palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color darkRedColor = Color(0xff9D3514);
+const Color darkBlueColor = Color(0x1f252fff);
+const Color lightBlueColor = Color(0xff4987C1);
+const Color lightFontsColor = Color(0xff474747);
